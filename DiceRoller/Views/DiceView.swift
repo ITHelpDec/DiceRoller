c78ce9d73eb566726e31131747af3f4de6333dc6
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-19 19:25:54 -0400

    Add DiceView SwiftUI file
      * Views now grouped into their own folder

---
+++ DiceRoller/Views/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by ITHelpDec on 19/05/2024.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
