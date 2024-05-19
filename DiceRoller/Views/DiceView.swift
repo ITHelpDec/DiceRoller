cc26059e097e142ab4bbcb16e356ebf12435732f
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-19 19:32:40 -0400

    Embed Image in VStack

--- DiceRoller/Views/DiceView.swift
+++ DiceRoller/Views/DiceView.swift
@@ -11,9 +11,11 @@ struct DiceView: View {
     var numberOfPips: Int = 1
     
     var body: some View {
-        Image(systemName: "die.face.\(numberOfPips)")
-            .resizable()
-            .frame(width: 100, height: 100)
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips)")
+                .resizable()
+                .frame(width: 100, height: 100)
+        }
     }
 }
 
