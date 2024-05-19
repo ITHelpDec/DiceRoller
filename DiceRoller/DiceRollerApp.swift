a1abb832bf8d97cd92c1569130fe57bf2606c82e
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-19 19:09:25 -0400

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by ITHelpDec on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
