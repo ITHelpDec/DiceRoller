a5d2076435fa7a239f68f7f755ae9a858c1a13c6
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-22 01:53:42 +0100

    Increase max dice from 3 to 5

--- DiceRoller/Views/ContentView.swift
+++ DiceRoller/Views/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                 Button("Add die") {
                     numberOfDice += 1
                 }
-                .disabled(numberOfDice == 3)
+                .disabled(numberOfDice == 5)
             }
             .padding()
         }
