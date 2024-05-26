8b4ee10f2c3b26906ae09945021c39cfb9b5c095
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-26 17:55:30 +0100

    Part 1 - Update the UI with state (#1)
      * Explore how @State properties and buttons work to update the UI of your app by creating an app to roll virtual dice
      * Add the functionality to increase or decrease the number of dice on the screen to play different kinds of games

---
+++ DiceRoller/Views/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by ITHelpDec on 19/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
+    var body: some View {
+        VStack {
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.description) { _ in
+                    DiceView()
+                }
+            }
+            
+            HStack {
+                Button("Remove die", systemImage: "minus.circle.fill") {
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add die", systemImage: "plus.circle.fill") {
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.appBackground)
+        .tint(.white)
+    }
+}
+
+#Preview {
+    ContentView()
+}
